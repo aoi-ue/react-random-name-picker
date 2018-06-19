edbd3f3996bafa5f100a3472fa203d33b00a04b2
yuhanyuhan <yuhanlian@gmail.com>
2018-06-19 19:08:10 +0800

    final submission

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
-import React, { Component } from 'react';
-import math from "mathjs" 
-import RandomButton from './RandomButton'
+import React, { Component } from "react";
+import math from "mathjs";
+import RandomButton from "./RandomButton";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state = {  
+    this.state = {
       names: ["gordon", "sahil", "david", "sally", "jane", "alice"],
-      luckyWinnerIndex: null
+      luckyWinnerIndex: null,
+      value: "",
+      blah: "hello"
     };
   }
+
+  handleChange = event => {
+    event.preventDefault();
+    this.setState({ value: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+
+    this.setState({
+      names: [...this.state.names, this.state.value]
+    });
+  };
+
   render() {
     return (
       <div>
-        <h1 id ="app-title">hello random name picker</h1>
+        <h1 id="app-title">hello random name picker</h1>
         <h3>
-          {this.state.names.map((name,i) => {
-            return (this.state.luckyWinnerIndex === i) ? 
-              <li id ="winner">{name}</li> :
-              <li id ="">{name}</li>
+          {this.state.names.map((name, i) => {
+            return this.state.luckyWinnerIndex === i ? (
+              <li id="winner" key={i}>{name}</li>) : 
+              (
+              <li id="" key={i}>{name}</li>);
           })}
         </h3>
-        <RandomButton handleClick = {()=>this.handleClick()}/> 
+
+        <RandomButton handleClick={() => this.handleClick()} />
+          <br /> 
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input
+              type="text"
+              value={this.state.blah}
+              onChange={(event) => this.setState({blah: event.target.value})}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
 
   handleClick() {
-    const numberOfNames = this.state.names.length
-    const randomInteger = math.randomInt(numberOfNames)
+    const numberOfNames = this.state.names.length;
+    const randomInteger = math.randomInt(numberOfNames);
     this.setState({
-      luckyWinnerIndex: randomInteger})
+      luckyWinnerIndex: randomInteger
+    });
   }
-
-} 
+}
 
 export default App;
